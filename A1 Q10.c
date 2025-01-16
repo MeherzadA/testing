10adc4677976d58fe0defbfbdee27c6962c1b1f1
MeherzadA <97406502+MeherzadA@users.noreply.github.com>
2025-01-15 20:17:43 -0500

    push try again other file

---
+++ A1 Q10.c
@@ -0,0 +1,25 @@
+int funcT(int x, int y, int input) {
+
+}
+
+
+
+int main () {
+
+
+
+    return 0;
+}
+
+int funcT(int x, int y, int input) {
+    
+    if(input == 1){
+        return x;
+    }
+
+    else if(input == 2){
+        return y;
+    }
+
+    
+}
